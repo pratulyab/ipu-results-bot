e59d04754bd2b25753bc6b327f6d366c28c64e6b
Pratulya Bubna <pratulyabubna@outlook.com>
2017-04-03 00:54:55 +0530

    DEBUG = True

--- resultsbot/settings.py
+++ resultsbot/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '42%+eyvmpszzqx2mn4$#gqu4ro#d)!^d)t$k@$df7lh=ofxdx#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
